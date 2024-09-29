6b2f40fb6e22b30b60304e770d99b7c68737c797
seojun <kevin2005102@gmail.com>
2024-09-30 01:11:49 +0900

    eos

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,86 @@
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-
-        appBar: AppBar(
-          backgroundColor: Colors.lightGreen,
-          title: const Text('EOS ToDoList'),
-          leading: Icon(Icons.check_box_outlined),
-        ),
-
-        body: Container(
-          height: 200,
-          color: Colors.lightGreen.withOpacity(0.3),
-          padding: EdgeInsets.all(25),
-
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(width: 10, color: Colors.grey),
-                  color: Colors.white, // color 속성을 여기에 포함
-                ),
-                width: 150,
-                height: 150,
-                child: Center(
-                  child: Icon(
-                    Icons.person,
-                    size: 100,
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-              SizedBox(width: 10),
-
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('EOS'),
-                  Text(
-                    '정서준',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                    ),
-                  ),
-                  Text('안녕하세요~'),
-                ]
-
-              )
-
-            ],
-          ),
-        ),
+      title: 'EOS ToDoList',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        fontFamily: 'Pretendard',
+        useMaterial3: true,
       ),
+      home: MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFFA4C639).withOpacity(0.1),
+          title: Text('EOS ToDoList'),
+          leading: Container(
+            padding: EdgeInsets.all(5),
+            child: Image.asset(
+              'assets/images/eos_logo.png',
+              fit: BoxFit.contain,
+            )
+          ),
+        ),
+        body: Column(
+          children: [
+            Container(
+                height: 200,
+                padding: EdgeInsets.all(25),
+                child: Row(children: [
+                  Container(
+                    width: 140,
+                    height: 140,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(width: 10, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(250),
+                    ),
+                    child: Center(
+                      child: Image.asset('assets/images/eos_logo.png'),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 35,
+                  ),
+                  Column (
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '정서준',
+                          style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text('공부 열심히 하기 !')
+                      ]
+
+                  )
+                ])),
+
+
+            Container(
+              height: 500,
+              padding: EdgeInsets.all(25),
+              color: Color(0xFFA4C639).withOpacity(0.1),
+            )
+          ],
+        ));
+  }
+}
